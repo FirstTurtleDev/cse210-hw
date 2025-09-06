a92782df0bae4031eabd268794521a5db18e166f
firstturtledev <alejandrovanderhorst@gmail.com>
2025-09-06 10:09:48 -0400

    Did exercise3 from week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int number = random.Next(1, 11);
+        int magicNumberGuess;
+        do
+        {
+            Console.Write("What is the magic number? ");
+            string magicInput = Console.ReadLine();
+            magicNumberGuess = int.Parse(magicInput);
+            if (magicNumberGuess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumberGuess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (magicNumberGuess != number);
     }
 }
