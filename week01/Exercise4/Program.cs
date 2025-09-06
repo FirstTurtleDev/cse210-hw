bfabbd3d3a8dd8082b1ea3a42838a31c58e36c4a
firstturtledev <alejandrovanderhorst@gmail.com>
2025-09-06 17:57:12 -0400

    week 1 exercise 1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
-}
+        int input = 1; 
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (input != 0)
+        {
+
+            Console.Write("Enter number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(input);
+        }
+        numbers.Remove(0);
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("The sum is " + sum);
+        Console.WriteLine("The average is " + (double)sum / numbers.Count);
+        int largest = 0;
+
+        if (numbers.Count > 0)
+        {
+            largest = numbers[0];
+        }
+
+        if (largest < 0)
+        {
+            largest = 0;
+        }
+
+        Console.WriteLine("The largest number is " + largest);    }
+        }
